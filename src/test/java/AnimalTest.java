03967ff6944dc1d6ae4b7c8780d50bfd662ec76f
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-06 13:25:56 +0300

    This commit adds the dependencies for the AnimalTest file

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -1,4 +1,13 @@
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class AnimalTest {
 
