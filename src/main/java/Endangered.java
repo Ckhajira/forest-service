2e0f0e6d91b56a3616bf820f334eb596f95703a4
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-07 16:59:49 +0300

    This commit adds the variables for the endangered class

--- src/main/java/Endangered.java
+++ src/main/java/Endangered.java
@@ -1,3 +1,11 @@
 public class Endangered{
-
+    private class Endangered implements DatabaseInterfaces{
+        private int id;
+        private String name;
+        private String danger;
+        private String health;
+        private String age;
+        private String location;
+        private String ranger;
+    }
 }
