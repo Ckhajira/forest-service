30c1ff8d1e0c1aaa1a840544193e933fa6af592f
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-11-07 17:20:13 +0300

    This commit adds the templates folder

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import static spark.Spark.*;
+
 public class App{
-    public static void main(){
+    public static void main(String[] args){
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 }
